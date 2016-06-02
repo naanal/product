f73a3c2fe6166df876c1941f30b85f98857bef43
rajagopalx <rajagopalx@gmail.com>
2016-06-02 18:16:17 +0530

    User Dashboard Basic Html Template

--- user/dashboard/openstack_dashboard/test/error_pages_urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import url
-from django.views import defaults
-
-from openstack_dashboard.urls import urlpatterns  # noqa
-
-urlpatterns.append(url(r'^500/$', defaults.server_error))
