3468e6751e720f1cdd7299c00d252dfe63600bee
shanmugava70 <shan.july.1994@gmail.com>
2016-06-06 10:38:01 +0530

    without change password

--- user/dashboard/Naanal_Userdashboard/login/views.py
+++ user/dashboard/Naanal_Userdashboard/login/views.py
@@ -65,11 +65,26 @@ def loginpage(request):
 def index_page(request):
     username = password = ''
     if request.GET:
-        username = request.GET.get('username')
-        #password = request.POST.get('password')
-        print "name"
-        print username
-        return render_to_response('index.html',{'password':password, 'username': username})   
+        username = request.GET.get('username')        
+        db = MySQLdb.connect(host="172.30.1.242",port=3306,user="root",passwd="password",db="mysql")
+        cursor = db.cursor()
+        cursor.execute("select instances_id from mapping1 where username='%s'" % (username))
+        results = cursor.fetchall()
+        for row in results:
+            instance_id = row[0]
+            print instance_id
+        db.commit()
+        db.close()
+        db = MySQLdb.connect(host="172.30.1.242",port=3306,user="root",passwd="password",db="nova")
+        cursor = db.cursor()
+        cursor.execute("select display_name,vm_state from instances where uuid='%s'" % (instance_id))
+        results = cursor.fetchall() 
+        for row in results:               
+            instance_name = row[0]
+            status =row[1]                
+        db.commit()
+        db.close()
+        return render_to_response('index.html',{'password':password, 'username': username,'instancename':instance_name,'instanceid':instance_id,'status':status})
     
     return render_to_response('index.html',{'password':password, 'username': username})
 
@@ -81,59 +96,48 @@ def change_password(request):
         username = request.GET.get('username')
         password = request.GET.get('password')
         #password = request.POST.get('password')
-        print "change method"
+        print "GET method"
         print username 
         return render_to_response('changepassword.html',{'password':password, 'username': username})       
     
-    if request.POST:        
-        password = request.POST.get('inputPassword1')
+    if request.POST:  
+        username = request.POST.get('username')      
+        password = request.POST.get('currentpassword')
+        new_password = request.POST.get('newpassword')
+        print username
         print password
+        print new_password
         print "change post method"
-        ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_NEVER)
-        l = ldap.initialize('ldaps://172.30.1.197')
-        l.simple_bind_s('Administrator@naanal.local', 'p@ssw0rd1') 
-        dn="cn=map1,dc=naanal,dc=local" 
-        password='p@ssw0rd1'
-        unicode_pass = unicode('\"' + password + '\"', 'iso-8859-1')
-        password_value = unicode_pass.encode('utf-16-le')
-        print (password_value)
-        unicode_pass = unicode('\"' + password + '\"', 'iso-8859-1')
-        password_value = unicode_pass.encode('utf-16-le')
-        add_pass = [(ldap.MOD_REPLACE, 'unicodePwd', [password_value])]
-        l.modify_s(dn, add_pass)
-        l.unbind_s()      
-        return render_to_response('changepassword.html',{'password':password, 'username': username})   
-    
+        LDAP_SERVER = 'ldap://172.30.1.197'
+        LDAP_USERNAME = '%s@naanal.local' % username
+        LDAP_PASSWORD = password
+        base_dn = 'DC=naanal,DC=local'
+        ldap_filter = 'userPrincipalName=%s@naanal.local' % username
+        attrs = ['memberOf']
+        try :
+             ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_NEVER)  
+             ldap_client = ldap.initialize(LDAP_SERVER)   
+             ldap_client.set_option(ldap.OPT_REFERRALS,0)
+             ldap_client.simple_bind_s(LDAP_USERNAME, LDAP_PASSWORD)
+             print 'user current password authentication successfull'  
+             return render_to_response('changepassword.html',{'password':password, 'username': username})   
+        except ldap.INVALID_CREDENTIALS:
+             ldap_client.unbind()
+             return render_to_response('changepassword.html',{'password':password, 'username': username}) 
+             print 'Wrong username or password'
+        except ldap.SERVER_DOWN:
+             print 'windows active directory not available'
+
     return render_to_response('changepassword.html',{'password':password, 'username': username})
 
 
-def help(request):    
-    
-    return render_to_response('help.html')
-@csrf_exempt
-def change_pswd(request):  
+def help(request):
     username = password = ''
-    print 'post method'
-    if request.POST:        
-        password = request.POST.get('inputPassword1')
-        print password
-        print "change post method"
-        ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_NEVER)
-        l = ldap.initialize('ldaps://172.30.1.197')
-        l.simple_bind_s('Administrator@naanal.local', 'p@ssw0rd1') 
-        dn="cn=map1,dc=naanal,dc=local" 
-        password='p@ssw0rd1'
-        unicode_pass = unicode('\"' + password + '\"', 'iso-8859-1')
-        password_value = unicode_pass.encode('utf-16-le')
-        print (password_value)
-        unicode_pass = unicode('\"' + password + '\"', 'iso-8859-1')
-        password_value = unicode_pass.encode('utf-16-le')
-        add_pass = [(ldap.MOD_REPLACE, 'unicodePwd', [password_value])]
-        l.modify_s(dn, add_pass)
-        l.unbind_s()      
-        return render_to_response('changepassword.html',{'password':password, 'username': username})   
-    
-    return render_to_response('changepassword.html',{'password':password, 'username': username})
+    if request.GET:
+        username = request.GET.get('username')        
+        print username     
+        return render_to_response('help.html',{'username': username})
+
 
 @csrf_exempt
 def instance_stop(request):
@@ -165,8 +169,7 @@ def instance_stop(request):
         elif operation == 'start':
             server.start()
         elif operation == 'reboot':
-            server.reboot()  # Needed to check this method
-        
+            server.reboot(reboot_type='SOFT')         
         #****************** MYSQL coding to check the instances status *************************************
         time.sleep(20)
         db = MySQLdb.connect(host="172.30.1.242",port=3306,user="root",passwd="password",db="nova")
@@ -182,3 +185,31 @@ def instance_stop(request):
         return render_to_response('index.html',{'password':password, 'username': username,'instancename':instance_name,'instanceid':instance_id,'status':status})
  
 
+
+
+
+
+@csrf_exempt
+def change_pswd(request):  
+    username = password = ''
+    print 'post method'
+    if request.POST:        
+        password = request.POST.get('inputPassword1')
+        print password
+        print "change post method"
+        ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_NEVER)
+        l = ldap.initialize('ldaps://172.30.1.197')
+        l.simple_bind_s('Administrator@naanal.local', 'p@ssw0rd1') 
+        dn="cn=map1,dc=naanal,dc=local" 
+        password='p@ssw0rd1'
+        unicode_pass = unicode('\"' + password + '\"', 'iso-8859-1')
+        password_value = unicode_pass.encode('utf-16-le')
+        print (password_value)
+        unicode_pass = unicode('\"' + password + '\"', 'iso-8859-1')
+        password_value = unicode_pass.encode('utf-16-le')
+        add_pass = [(ldap.MOD_REPLACE, 'unicodePwd', [password_value])]
+        l.modify_s(dn, add_pass)
+        l.unbind_s()      
+        return render_to_response('changepassword.html',{'password':password, 'username': username})   
+    
+    return render_to_response('changepassword.html',{'password':password, 'username': username})
